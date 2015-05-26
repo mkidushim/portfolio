a4fe2b524376bbd8f2a6d3a557b564e9724950c7
mkidushim <mkidushim@gmail.com>
2015-05-25 22:08:16 -0700

    Finished Portfolio_2.0

--- portfolio.js
+++ portfolio.js
@@ -7,7 +7,8 @@ function email() {
     $('#email').text('E-mail' + ':' + ' ' + (email_name + server_name));
 
 }
-function show_r (){
+
+function show_r() {
     $('').css
 }
 
@@ -35,11 +36,35 @@ $(document).ready(function() {
             }
         });
     })
-    $('body').on('click','.resume > span', function(){
+    $('body').on('touchstart', '.home', function() {
+        console.log('Home btn works')
+        $.ajax({
+            url: 'home.html',
+            dataType: 'html',
+            cache: false,
+            success: function(response) {
+                console.log('success:', response)
+                $('.main_content').html(response)
+            }
+        });
+    })
+    $('body').on('click', '.resume > span', function() {
         console.log('hover')
         $('.ss_img').toggleClass('show')
     })
     $('body').on('click', '.about', function() {
+        console.log('Home btn works')
+        $.ajax({
+            url: 'about.html',
+            dataType: 'html',
+            cache: false,
+            success: function(response) {
+                console.log('success:', response)
+                $('.main_content').html(response)
+            }
+        });
+    })
+    $('body').on('touchstart', '.about', function() {
         console.log('Home btn works')
         $.ajax({
             url: 'about.html',
@@ -63,7 +88,31 @@ $(document).ready(function() {
             }
         });
     })
+    $('body').on('touchstart', '.skills', function() {
+        console.log('Home btn works')
+        $.ajax({
+            url: 'skills.html',
+            dataType: 'html',
+            cache: false,
+            success: function(response) {
+                console.log('success:', response)
+                $('.main_content').html(response)
+            }
+        });
+    })
     $('body').on('click', '.contact', function() {
+        console.log('Home btn works')
+        $.ajax({
+            url: 'contact.html',
+            dataType: 'html',
+            cache: false,
+            success: function(response) {
+                console.log('success:', response)
+                $('.main_content').html(response)
+            }
+        });
+    })
+    $('body').on('touchstart', '.contact', function() {
         console.log('Home btn works')
         $.ajax({
             url: 'contact.html',
